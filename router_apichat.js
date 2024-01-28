b3dbc37820f5a6efd288eccb71fcb403a1a427c1
IceKungzz <147721215+IceKungzz@users.noreply.github.com>
2024-01-28 11:46:21 +0700

    Update router_apichat.js

--- router_apichat.js
+++ router_apichat.js
@@ -1,44 +1,44 @@
-const express = require('express');
-const router = express.Router();
-const { MongoClient } = require('mongodb');
-const mongodb = process.env.mongoURI;
-const io = require('socket.io'); // Import io instance
+// const express = require('express');
+// const router = express.Router();
+// const { MongoClient } = require('mongodb');
+// const mongodb = process.env.mongoURI;
+// const io = require('socket.io'); // Import io instance
 
-router.post('/chat', async (req, res) => {
-    let client;
-    try {
-        client = new MongoClient(mongodb);
-        await client.connect();
+// router.post('/chat', async (req, res) => {
+//     let client;
+//     try {
+//         client = new MongoClient(mongodb);
+//         await client.connect();
         
-        const db = client.db('Api-nodejs');
-        const collection = db.collection('chat-messages');
+//         const db = client.db('Api-nodejs');
+//         const collection = db.collection('chat-messages');
 
-        // io socket
-        io.on('connection', async (socket) => {
-            console.log('user connected');
+//         // io socket
+//         io.on('connection', async (socket) => {
+//             console.log('user connected');
 
-            socket.on('chat message', async (message) => {
-                const datamessage = {
-                    fname: socket.fname,
-                    message: message,
-                    time: socket.time
-                }
-                const result = await collection.insertOne(datamessage);
-                console.log('Message = ', result);
-                io.emit('chat message', message);
-            });
+//             socket.on('chat message', async (message) => {
+//                 const datamessage = {
+//                     fname: socket.fname,
+//                     message: message,
+//                     time: socket.time
+//                 }
+//                 const result = await collection.insertOne(datamessage);
+//                 console.log('Message = ', result);
+//                 io.emit('chat message', message);
+//             });
 
-            socket.on('disconnect', () => {
-                console.log('user disconnected');
-            })
-        });
-    } catch (err) {
-        res.status(500).json({ status: "error", err })
-    } finally {
-        if (client) {
-            client.close();
-        }
-    }
-});
+//             socket.on('disconnect', () => {
+//                 console.log('user disconnected');
+//             })
+//         });
+//     } catch (err) {
+//         res.status(500).json({ status: "error", err })
+//     } finally {
+//         if (client) {
+//             client.close();
+//         }
+//     }
+// });
 
-module.exports = router;
+// module.exports = router;
